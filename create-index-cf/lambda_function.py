1e68386352cfc50bbc8f2708f400a5ad14a6c1fe
Hongyi Li <hongyi.li@sezzle.com>
2022-11-13 20:05:51 -0500

    Build bot upon creation

--- create-index-cf/lambda_function.py
+++ create-index-cf/lambda_function.py
@@ -1,6 +1,7 @@
 from opensearchpy import OpenSearch, RequestsHttpConnection
 import os
 import cfnresponse
+import boto3
 
 index_name = "photos"
 openSearchHost = os.environ['openSearchHost']
@@ -12,6 +13,11 @@ openSearch = OpenSearch(
     connection_class=RequestsHttpConnection,
 )
 
+lex = boto3.client('lexv2-models')
+botId = os.environ['lexBotID']
+botAliasId = 'TSTALIASID'
+localeId = 'en_US'
+
 def create_index():
     # Create an index with non-default settings.
     index_body = {"settings": {"index": {"number_of_shards": 1}}}
@@ -21,10 +27,17 @@ def create_index():
         response = e
     return response
 
+def build_bot():
+    lex.build_bot_locale(
+        botId=botId,
+        botVersion='DRAFT',
+        localeId=localeId
+    )
 
 def lambda_handler(event, context):
     # TODO implement
     if event["RequestType"] == "Create":
         create_index()
+        build_bot()
     cfnresponse.send(event, context, cfnresponse.SUCCESS, {"result": "ok"})
     return "ok"
